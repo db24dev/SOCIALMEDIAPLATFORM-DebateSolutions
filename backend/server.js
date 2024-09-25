c05789a09e52dee198c252f59e33520ad49dbe95
Dario Berretta <darioberretta24@gmail.com>
2024-09-24 21:20:52 -0700

    1 - auth controller added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from './db/connectMongoDB.js';
@@ -9,7 +10,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use("/api/auth",authRoutes);
+app.use(express.json());  // to parse req.body
+app.use(express.urlencoded({ extended: true }));  // to parse form data(urlencoded)
+
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
