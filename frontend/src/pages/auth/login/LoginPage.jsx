04ae5d7f125bf54f47fceaf4ff50d07dd4792da5
Dario Berretta <darioberretta24@gmail.com>
2024-10-16 21:58:51 -0700

    3- Auth handled and routes protected in client

--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -6,26 +6,58 @@ import XSvg from "../../../components/svgs/X";
 import { MdOutlineMail } from "react-icons/md";
 import { MdPassword } from "react-icons/md";
 
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
 const LoginPage = () => {
-	const [formData, setFormData] = useState({
-		username: "",
-		password: "",
-	});
+  const [formData, setFormData] = useState({
+	username: "",
+	password: "",
+  });
+  const queryClient = useQueryClient();
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		console.log(formData);
-	};
+  const {
+	mutate:loginMutation,
+	isPending,
+	isError,
+	error,
+  } = useMutation({
+	mutationFn: async ({ username, password }) => {
+	  try {
+		const res = await fetch("api/auth/login", {
+		  method: "POST",
+		  headers: {
+			"Content-Type": "application/json",	
+		  },
+		  body: JSON.stringify({ username, password }),
+		});
+		  
+		const data = await res.json();
 
-	const handleInputChange = (e) => {
-		setFormData({ ...formData, [e.target.name]: e.target.value });
-	};
+		if (!res.ok) {
+		  throw new Error(data.error || "Something went wrong");
+		}
+	  } catch (error) {
+		throw new Error(error);	
+	  }
+	},
+	onSuccess: () => {
+	  // refetch the authUser query to update the UI
+	  queryClient.invalidateQueries({ queryKey: ["authUser"] });
+	},	
+  });
 
-	const isError = false;
+  const handleSubmit = (e) => {
+	e.preventDefault();
+	loginMutation(formData);
+  };
 
-	return (
-		<div className='max-w-screen-xl mx-auto flex h-screen'>
-			<div className='flex-1 hidden lg:flex items-center  justify-center'>
+  const handleInputChange = (e) => {
+	setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  return (
+	<div className='max-w-screen-xl mx-auto flex h-screen'>
+	  <div className='flex-1 hidden lg:flex items-center  justify-center'>
 				<XSvg className='lg:w-2/3 fill-white' />
 			</div>
 			<div className='flex-1 flex flex-col justify-center items-center'>
@@ -55,8 +87,12 @@ const LoginPage = () => {
 							value={formData.password}
 						/>
 					</label>
-					<button className='btn rounded-full btn-primary text-white'>Login</button>
-					{isError && <p className='text-red-500'>Something went wrong</p>}
+					<button className='btn rounded-full btn-primary text-white'>
+					  {isPending ? "Loading..." : "Login"}	
+					</button>
+					{isError && <p className='text-red-500'>
+						{error.message}
+						</p>}
 				</form>
 				<div className='flex flex-col gap-2 mt-4'>
 					<p className='text-white text-lg'>{"Don't"} have an account?</p>
