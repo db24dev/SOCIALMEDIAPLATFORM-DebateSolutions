ab5f82ee8cddb1355f7c6d3b521793ea174fa4e7
Dario Berretta <darioberretta24@gmail.com>
2024-10-22 02:15:00 -0700

    3- userFollow hook added

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -45,7 +45,7 @@ export const followUnfollowUser = async (req, res) => {
         // });
 
         // await newNotification.save();
-        // TODO: return the id of the user as a response
+        
         res.status(200).json({ message: "User unfollowed successfully" });
       } else{
         // Follow the user
@@ -60,7 +60,6 @@ export const followUnfollowUser = async (req, res) => {
 
         await newNotification.save();
 
-        // TODO: return the id of the user as a response
         res.status(200).json({ message: "User followed successfully" });
       }
     } catch (error) {
