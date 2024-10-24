b3ea0edf185984582290ef95c032e3f6144e59fe
Dario Berretta <darioberretta24@gmail.com>
2024-10-23 17:11:26 -0700

    3- Profile page posts and user info fetched

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -3,37 +3,45 @@ import PostSkeleton from "../skeletons/PostSkeleton";
 import { useQuery } from '@tanstack/react-query';
 import { useEffect } from "react";
 
-const Posts = ({ feedType }) => {
-
-	const getPostEndpoint = () => {
+const Posts = ({ feedType, username, userId }) => {
+    const getPostEndpoint = () => {
 		switch (feedType) {
 		  case "forYou":
-			return "api/posts/all";	
+			return "/api/posts/all";	
 		  case "following":
-			return "api/posts/following";
+			return "/api/posts/following";
+		  case "posts":
+			return `/api/posts/user/${username}`;
+		  case "likes":
+			return `/api/posts/likes/${userId}`; 
 		  default:
-			return "api/posts/all";		
+			return "/api/posts/all";		
 		}
 	};
 
 	const POST_ENDPOINT = getPostEndpoint();
 
-	const {data:posts, isLoading, refetch, isRefetching } = useQuery({
-		queryKey: ["posts"],
-		queryFn: async () => {
-			try {
-			  const res = await fetch(POST_ENDPOINT);
-			  const data = await res.json();
+	const {
+	  data:posts, 
+	  isLoading, 
+	  refetch, 
+	  isRefetching,
+	} = useQuery({
+	  queryKey: ["posts"],
+	  queryFn: async () => {
+		try {
+		  const res = await fetch(POST_ENDPOINT);
+		  const data = await res.json();
 			  
-			  if(!res.ok){
-				throw new Error(data.error || "Something went wrong");
-			  } 
+		  if(!res.ok){
+			throw new Error(data.error || "Something went wrong");
+		  } 
 
-			  return data;
+			return data;
 				
-			} catch (error) {
-			  throw new Error(error);	
-			}
+		} catch (error) {
+			throw new Error(error);	
+		}
 			
 		},
 	});
@@ -41,7 +49,7 @@ const Posts = ({ feedType }) => {
 	useEffect(() => {
 		refetch();
 
-	}, [feedType, refetch]);
+	}, [feedType, refetch, username]);
 
 	return (
 		<>
